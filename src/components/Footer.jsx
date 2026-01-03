0c138cb5961b2bf476c7e1112cc16adedc0465ca
Chathumini Priyasha Rathnayaka <chathumini.20241955@iit.ac.lk>
2026-01-03 23:31:13 +0530

    Modified the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,50 @@
-function Footer() {
+/*function Footer() {
     return (
         <footer className="footer">
             <p>@ 2025 PropertyFinder</p>
         </footer>
     );
+}
+
+export default Footer; */
+
+
+
+
+import { Link } from "react-router-dom";
+import "../styles/App.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-top">
+        <div className="footer-column">
+          <h4>Buy</h4>
+          <Link to="/search?mode=buy">Houses for Sale</Link>
+          <Link to="/search?mode=buy">Flats for Sale</Link>
+        </div>
+        <div className="footer-column">
+          <h4>Rent</h4>
+          <Link to="/search?mode=rent">Houses for Rent</Link>
+          <Link to="/search?mode=rent">Flats for Rent</Link>
+        </div>
+        <div className="footer-column">
+          <h4>Sold</h4>
+          <Link to="/search?mode=sold">Sold houses prices</Link>
+          <Link to="/search?mode=sold">Instant valuation</Link>
+        </div>
+        <div className="footer-column">
+          <h4>Help</h4>
+          <Link to="/faq">FAQ</Link>
+          <Link to="/support">Support</Link>
+        </div>
+      </div>
+
+      <div className="footer-bottom">
+        <p>&copy; 2025 HomeSearch. All rights reserved.</p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
