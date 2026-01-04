8f6683794da9c603643a62dc56a8b08f36ac72ea
Chathumini Priyasha Rathnayaka <chathumini.20241955@iit.ac.lk>
2026-01-04 14:03:32 +0530

    React tabs on Property Page

--- src/pages/PropertyPage.jsx
+++ src/pages/PropertyPage.jsx
@@ -1,94 +1,60 @@
-/*import { useParams } from "react-router-dom";
-import data  from "../data/properties.json";
-import PropertyGallery from "../components/PropertyGallery";
-
-function PropertyPage() {
-    const { id } = useParams();
-    const property = data.properties.find(p => p.id === id);
-
-    if(!property) {
-        return <p>Property not found</p>;
-    }
-
-    return (
-        <div className="container">
-            <h1>£{property.price.toLocaleString()}</h1>
-            <h2>{property.location}</h2>
-
-            {/* Image gallery 
-            <div className="property-gallery">
-                {property.picture.map((img, index) => (
-                    <img 
-                        key={index}
-                        src={img}
-                        alt={`Property ${index +1}`}
-                    />
-                ))}
-            </div>
-
-            {/* Property details 
-            <p><strong>Type:</strong> {property.type}</p>
-            <p><strong>Bedrooms:</strong> {property.bedrooms}</p>
-            <p><strong>Tenture:</strong> {property.tenture}</p>
-
-            {/* Floor plan 
-            <h3>Floor Plan</h3>
-            <img
-                src={property.floorPlan}
-                alt="Property floor plan"
-                className="floorplan-image"
-            />
-
-            {/* Description 
-            <p>{property.longDescription}</p>
-        </div>
-    );
-}
-
-export default PropertyPage;*/
-
-
-
 import { useParams } from "react-router-dom";
 import data from "../data/properties.json";
 import PropertyGallery from "../components/PropertyGallery";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 function PropertyPage() {
   const { id } = useParams();
-  const property = data.properties.find(p => p.id === id);
+  const property = data.properties.find((p) => p.id === id);
 
-  if (!property) {
-    return <p>Property not found</p>;
-  }
+  if (!property) return <p>Property not found</p>;
 
   return (
     <div className="container">
       <h1>£{property.price.toLocaleString()}</h1>
       <h2>{property.location}</h2>
 
-      {/* Image Gallery */}
       <PropertyGallery images={property.picture} />
 
-      {/* Property details */}
       <p><strong>Type:</strong> {property.type}</p>
       <p><strong>Bedrooms:</strong> {property.bedrooms}</p>
       <p><strong>Tenure:</strong> {property.tenure}</p>
 
-      <h3>Description</h3>
-      <p>{property.longDescription}</p>
+      <Tabs>
+        <TabList>
+          <Tab>Description</Tab>
+          <Tab>Floor Plan</Tab>
+          <Tab>Google Map</Tab>
+        </TabList>
 
-      <h3>Floor Plan</h3>
-      <img
-        src={property.floorPlan}
-        alt="Property floor plan"
-        className="floorplan-image"
-      />
+        <TabPanel>
+          <p>{property.longDescription}</p>
+        </TabPanel>
+        <TabPanel>
+          <img
+            src={property.floorPlan}
+            alt="Floor Plan"
+            className="floorplan-image"
+          />
+        </TabPanel>
+        <TabPanel>
+          <iframe
+            title="Property Map"
+            width="100%"
+            height="400"
+            style={{ border: 0, borderRadius: "8px" }}
+            loading="lazy"
+            allowFullScreen
+            src={`https://www.openstreetmap.org/export/embed.html?bbox=${property.lng-0.01}%2C${property.lat-0.01}%2C${property.lng+0.01}%2C${property.lat+0.01}&layer=mapnik&marker=${property.lat}%2C${property.lng}`}
+          ></iframe>
+        </TabPanel>
+      </Tabs>
     </div>
   );
 }
 
 export default PropertyPage;
-
 
 
 
